9ceeff51caf0051fd8cb0188801f2961e7741568
Shingo.sasaki <shingo.sasaki@studist.jp>
2021-09-19 21:50:53 +0900

    PC用のサイドメニューのレイアウトを概ね完了

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,21 @@
 import 'modern-css-reset'
+import Image from 'next/image'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <div className="app">
-        <nav className="nav">メインメニュー</nav>
+        <nav className="nav">
+          <Image src="/icon.jpg" alt="icon_shingo_sasaki" className="icon" width={160} height={160} />
+          <ul className="menu">
+            <li className="menu-item">ABOUT</li>
+            <li className="menu-item">EXPERIENCE</li>
+            <li className="menu-item">SKILLS</li>
+            <li className="menu-item selected">OUTPUTS</li>
+            <li className="menu-item">INTERSETS</li>
+            <li className="menu-item">CERTIFICATIONS</li>
+          </ul>
+        </nav>
         <main className="main">
           <Component {...pageProps} />
         </main>
@@ -17,9 +28,32 @@ function MyApp({ Component, pageProps }) {
             flex-direction: row;
 
             .nav {
+              display: flex;
+              flex-direction: column;
+              justify-content: center;
+              align-items: center;
               min-width: 270px;
               height: 100vh;
               background-color: rgb(189, 93, 56);
+
+              :global(.icon) {
+                border: 0.5rem solid rgba(255, 255, 255, 0.2) !important;
+                border-radius: 50%;
+              }
+
+              .menu {
+                padding: 0;
+                text-align: center;
+                .menu-item {
+                  height: 40px;
+                  color: rgba(255, 255, 255, 0.55);
+                  font-weight: 800;
+                  list-style: none;
+                  &.selected {
+                    color: #fff;
+                  }
+                }
+              }
             }
             .main {
               width: 100%;
