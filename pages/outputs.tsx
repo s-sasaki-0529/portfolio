06eb1dd35c3d12ad349a6e29a6d525724146f924
Shingo.sasaki <shingo.sasaki@studist.jp>
2021-09-20 23:30:32 +0900

    RSS でブログ記事を取得する下準備

--- pages/outputs.tsx
+++ pages/outputs.tsx
@@ -1,3 +1,36 @@
-export default function Outputs() {
-  return <div>Outputs</div>
+/* eslint-disable @next/next/no-img-element */
+import { GetStaticProps } from 'next'
+import RSSParser from 'rss-parser'
+
+type RSSFeed = {
+  zenn: RSSParser.Item[]
+  medium: RSSParser.Item[]
+}
+
+export const getStaticProps: GetStaticProps<RSSFeed> = async () => {
+  const rssParser = new RSSParser()
+  const zenn = await rssParser.parseURL('https://zenn.dev/sa2knight/feed').then(feed => feed.items)
+  const medium = await rssParser.parseURL('https://medium.com/@shingo.sasaki/feed').then(feed => feed.items)
+
+  return {
+    props: { zenn, medium }
+  }
+}
+
+export default function Outputs(props: RSSFeed) {
+  console.log(props)
+  return (
+    <div className="root">
+      {props.zenn.map(zennItem => (
+        <img key={zennItem.guid} alt="zenn_enclosure" src={zennItem.enclosure.url} />
+      ))}
+      <style jsx lang="scss">{`
+        img {
+          width: 300px;
+          height: 300px;
+          object-fit: scale-down;
+        }
+      `}</style>
+    </div>
+  )
 }
