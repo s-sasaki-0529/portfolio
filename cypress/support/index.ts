716129a8699d9aecaeec6072ddc5664646d12273
Shingo.sasaki <shingo.sasaki@studist.jp>
2021-09-29 21:26:47 +0900

    不要になったファイル削除

--- cypress/support/index.ts
+++
@@ -1,8 +0,0 @@
-Cypress.Commands.add('screenshotWithWaitImage', () => {
-  cy.get('img').should($img => {
-    if ($img) {
-      expect($img[0].naturalWidth, 'image has natural width').to.be.greaterThan(0)
-    }
-  })
-  cy.screenshot()
-})
