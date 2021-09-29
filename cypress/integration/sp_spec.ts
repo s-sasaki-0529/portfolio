9292fc4f266028453daa2b7515f51dd5630ccb79
Shingo.sasaki <shingo.sasaki@studist.jp>
2021-09-29 21:47:06 +0900

    SPのE2Eテストを強化

--- cypress/integration/sp_spec.ts
+++ cypress/integration/sp_spec.ts
@@ -26,4 +26,48 @@ describe('SPビュー', () => {
       cy.screenshot()
     })
   })
+
+  describe('outputs ページ', () => {
+    beforeEach(() => cy.visit('/outputs'))
+    it('カテゴリをクリックすることで切り替えることができる', () => {
+      cy.get('.medium-outputs')
+
+      cy.get('.categories').contains('Zenn').click()
+      cy.get('.zenn-outputs')
+      cy.wait(1000).screenshot()
+
+      cy.get('.categories').contains('Slideshare').click()
+      cy.get('.slideshare-outputs')
+      cy.wait(1000).screenshot()
+
+      cy.get('.categories').contains('Medium').click()
+      cy.get('.medium-outputs')
+      cy.screenshot()
+    })
+  })
+
+  describe('experience ページ', () => {
+    // FIXME: React のアタッチを待機してるけど、自動待機に 定評のある Cypress でこれいる？
+    beforeEach(() => cy.visit('/experience').wait(100))
+
+    it('各職歴の詳細を開閉できる', () => {
+      cy.get('.experience-block.saas .summary').click()
+      cy.get('.experience-block.saas .description').should('be.visible')
+      cy.screenshot()
+      cy.get('.experience-block.saas .summary').click()
+      cy.get('.experience-block.saas .description').should('be.not.visible')
+
+      cy.get('.experience-block.ses .summary').click()
+      cy.get('.experience-block.ses .description').should('be.visible')
+      cy.screenshot()
+      cy.get('.experience-block.ses .summary').click()
+      cy.get('.experience-block.ses .description').should('be.not.visible')
+
+      cy.get('.experience-block.isp .summary').click()
+      cy.get('.experience-block.isp .description').should('be.visible')
+      cy.screenshot()
+      cy.get('.experience-block.isp .summary').click()
+      cy.get('.experience-block.isp .description').should('be.not.visible')
+    })
+  })
 })
