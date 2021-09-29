72a521f9f91a2c4cbd7bb505f91c139e22adf6da
Shingo.sasaki <shingo.sasaki@studist.jp>
2021-09-29 21:13:56 +0900

    待機してみる

--- cypress/integration/pc_spec.ts
+++ cypress/integration/pc_spec.ts
@@ -2,24 +2,27 @@ describe('PCビュー', () => {
   beforeEach(() => cy.viewport(1280, 720))
 
   describe('メインメニュー', () => {
-    beforeEach(() => cy.visit('/'))
+    beforeEach(() => {
+      cy.visit('/')
+      cy.get('pc-nav .icon').should('be.visible')
+    })
 
     it('4種類のメニューを押下するとそれぞれのページにリンクする', () => {
       cy.contains('skills').click()
       cy.url().should('include', '/skills')
-      cy.screenshotWithWaitImage()
+      cy.screenshot()
 
       cy.contains('outputs').click()
       cy.url().should('include', '/outputs')
-      cy.screenshotWithWaitImage()
+      cy.screenshot()
 
       cy.contains('experience').click()
       cy.url().should('include', '/experience')
-      cy.screenshotWithWaitImage()
+      cy.screenshot()
 
       cy.contains('about').click()
       cy.url().should('include', '/about')
-      cy.screenshotWithWaitImage()
+      cy.screenshot()
     })
   })
 
@@ -30,15 +33,15 @@ describe('PCビュー', () => {
 
       cy.get('.categories').contains('Zenn').click()
       cy.get('.zenn-outputs')
-      cy.screenshotWithWaitImage()
+      cy.screenshot()
 
       cy.get('.categories').contains('Slideshare').click()
       cy.get('.slideshare-outputs')
-      cy.screenshotWithWaitImage()
+      cy.screenshot()
 
       cy.get('.categories').contains('Medium').click()
       cy.get('.medium-outputs')
-      cy.screenshotWithWaitImage()
+      cy.screenshot()
     })
   })
 
@@ -49,19 +52,19 @@ describe('PCビュー', () => {
     it('各職歴の詳細を開閉できる', () => {
       cy.get('.experience-block.saas .summary').click()
       cy.get('.experience-block.saas .description').should('be.visible')
-      cy.screenshotWithWaitImage()
+      cy.screenshot()
       cy.get('.experience-block.saas .summary').click()
       cy.get('.experience-block.saas .description').should('be.not.visible')
 
       cy.get('.experience-block.ses .summary').click()
       cy.get('.experience-block.ses .description').should('be.visible')
-      cy.screenshotWithWaitImage()
+      cy.screenshot()
       cy.get('.experience-block.ses .summary').click()
       cy.get('.experience-block.ses .description').should('be.not.visible')
 
       cy.get('.experience-block.isp .summary').click()
       cy.get('.experience-block.isp .description').should('be.visible')
-      cy.screenshotWithWaitImage()
+      cy.screenshot()
       cy.get('.experience-block.isp .summary').click()
       cy.get('.experience-block.isp .description').should('be.not.visible')
     })
