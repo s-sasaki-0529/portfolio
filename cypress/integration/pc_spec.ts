2710aa0d6d4490a8c5f1b4bcddd42c177020c5d3
Shingo.sasaki <shingo.sasaki@studist.jp>
2021-09-29 19:44:59 +0900

    Cypress 実行時にスクリーンショットを取るように

--- cypress/integration/pc_spec.ts
+++ cypress/integration/pc_spec.ts
@@ -7,15 +7,19 @@ describe('PCビュー', () => {
     it('4種類のメニューを押下するとそれぞれのページにリンクする', () => {
       cy.contains('skills').click()
       cy.url().should('include', '/skills')
+      cy.screenshot()
 
       cy.contains('outputs').click()
       cy.url().should('include', '/outputs')
+      cy.screenshot()
 
       cy.contains('experience').click()
       cy.url().should('include', '/experience')
+      cy.screenshot()
 
       cy.contains('about').click()
       cy.url().should('include', '/about')
+      cy.screenshot()
     })
   })
 
@@ -26,12 +30,15 @@ describe('PCビュー', () => {
 
       cy.get('.categories').contains('Zenn').click()
       cy.get('.zenn-outputs')
+      cy.screenshot()
 
       cy.get('.categories').contains('Slideshare').click()
       cy.get('.slideshare-outputs')
+      cy.screenshot()
 
       cy.get('.categories').contains('Medium').click()
       cy.get('.medium-outputs')
+      cy.screenshot()
     })
   })
 
@@ -42,16 +49,19 @@ describe('PCビュー', () => {
     it('各職歴の詳細を開閉できる', () => {
       cy.get('.experience-block.saas .summary').click()
       cy.get('.experience-block.saas .description').should('be.visible')
+      cy.screenshot()
       cy.get('.experience-block.saas .summary').click()
       cy.get('.experience-block.saas .description').should('be.not.visible')
 
       cy.get('.experience-block.ses .summary').click()
       cy.get('.experience-block.ses .description').should('be.visible')
+      cy.screenshot()
       cy.get('.experience-block.ses .summary').click()
       cy.get('.experience-block.ses .description').should('be.not.visible')
 
       cy.get('.experience-block.isp .summary').click()
       cy.get('.experience-block.isp .description').should('be.visible')
+      cy.screenshot()
       cy.get('.experience-block.isp .summary').click()
       cy.get('.experience-block.isp .description').should('be.not.visible')
     })
