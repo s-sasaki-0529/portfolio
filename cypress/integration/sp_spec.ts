8efd3a96dddd88a016a693663b563a8a1fdcbc05
Shingo.sasaki <shingo.sasaki@studist.jp>
2021-09-29 13:12:25 +0900

    一通りテストを作成

---
+++ cypress/integration/sp_spec.ts
@@ -0,0 +1,25 @@
+describe('PCビュー', () => {
+  beforeEach(() => cy.viewport(375, 812))
+
+  describe('メインメニュー', () => {
+    beforeEach(() => cy.visit('/').wait(50))
+
+    it('ハンバーガーメニューの開閉してそれぞれのページにリンクできる', () => {
+      cy.get('.sp-nav .hamburger').click()
+      cy.get('.sp-nav .menu').contains('skills').click()
+      cy.url().should('include', '/skills')
+
+      cy.get('.sp-nav .hamburger').click()
+      cy.get('.sp-nav .menu').contains('outputs').click()
+      cy.url().should('include', '/outputs')
+
+      cy.get('.sp-nav .hamburger').click()
+      cy.get('.sp-nav .menu').contains('experience').click()
+      cy.url().should('include', '/experience')
+
+      cy.get('.sp-nav .hamburger').click()
+      cy.get('.sp-nav .menu').contains('about').click()
+      cy.url().should('include', '/about')
+    })
+  })
+})
