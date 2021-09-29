f7321186bcc927d5590f12dd319d53f36e2335f7
Shingo.sasaki <shingo.sasaki@studist.jp>
2021-09-29 21:05:54 +0900

    E2EテストのSPビューが通らない問題を修正

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -1,8 +1,8 @@
 Cypress.Commands.add('screenshotWithWaitImage', () => {
-  cy.get('img')
-    .should('be.visible')
-    .and($img => {
+  cy.get('img').should($img => {
+    if ($img) {
       expect($img[0].naturalWidth, 'image has natural width').to.be.greaterThan(0)
-    })
+    }
+  })
   cy.screenshot()
 })
