1ddc59fa52e23504ed955ecb5ad158e12e744501
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2024-01-14 23:02:18 +0900

    _app.tsx から styled-jsx を削除

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -39,134 +39,46 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      <div className="app">
+      <div className="app flex">
         <Head>
           <title>Shingo Sasaki</title>
         </Head>
-        <nav className="pc-nav">
-          <Image src="/icon.jpg" alt="icon_shingo_sasaki" className="icon" width={160} height={160} />
-          <div className="menu p-2">
+        <nav className="flex flex-col justify-center items-center min-w-[270px] h-dvh bg-[rgb(189,_93,_56)] sp:hidden">
+          <Image
+            src="/icon.jpg"
+            alt="icon_shingo_sasaki"
+            className="!border-[0.5rem] !border-solid !border-[rgba(255,255,255,0.2)] rounded-[50%]"
+            width={160}
+            height={160}
+          />
+          <div className="m-[2rem] text-center">
             <Menu />
           </div>
         </nav>
-        <nav className="sp-nav">
-          <div className="header">
-            <h1 className="name">Shingo Sasaki</h1>
-            <button title="メインメニュー" className="hamburger" onClick={() => setIsShowSpMenu(!isShowSpMenu)}>
-              <FontAwesomeIcon icon={faBars} />
+        <nav className="sp-nav pc:hidden fixed left-0 right-0 top-0 z-[100]">
+          <div className="flex justify-between items-center w-dvw h-[56px] py-[8px] px-[16px] bg-[rgb(189,_93,_56)]">
+            <h1 className="text-white text-[1.25rem] font-light">Shingo Sasaki</h1>
+            <button
+              title="メインメニュー"
+              className="w-[56px] h-[40px] bg-transparent border-[1px] border-solid border-[rgba(255,_255,_255,_0.1)] rounded-[0.25em] cursor-pointer p-0"
+              onClick={() => setIsShowSpMenu(!isShowSpMenu)}
+            >
+              {' '}
+              <FontAwesomeIcon className="text-[rgba(255,_255,_255,_0.55)]" icon={faBars} />
             </button>
           </div>
-          <div className={isShowSpMenu ? 'menu' : 'menu collapsed'}>
+          <div
+            className={`fixed top-[56px] px-[16px] py-[0] w-full h-[160px] bg-[rgb(189,_93,_56)] [transition:height_0.1s] ${
+              isShowSpMenu ? '' : 'h-0 overflow-hidden'
+            }`}
+          >
             <Menu />
           </div>
         </nav>
-        <main className="main">
+        <main className="pc:flex pc:items-center pc:w-full pc:h-dvh pc:overflow-y-auto pc:py-[5rem] pc:px-[3rem] sp:w-dvw sp:mt-[56px] sp:text-[calc(0.5rem+1.5vw)] sp:p-[2em] sp:min-h-[calc(100vh-56px)]">
           <Component {...pageProps} />
         </main>
       </div>
-
-      <style jsx lang="scss">{`
-        .app {
-          display: flex;
-          @media screen and (min-width: 920px) {
-            flex-direction: row;
-
-            .pc-nav {
-              display: flex;
-              flex-direction: column;
-              justify-content: center;
-              align-items: center;
-              min-width: 270px;
-              height: 100vh;
-              background-color: rgb(189, 93, 56);
-
-              :global(.icon) {
-                border: 0.5rem solid rgba(255, 255, 255, 0.2) !important;
-                border-radius: 50%;
-              }
-              .menu {
-                margin: 2rem;
-                text-align: center;
-              }
-            }
-            .sp-nav {
-              display: none;
-            }
-            .main {
-              display: flex;
-              align-items: center;
-              width: 100%;
-              height: 100vh;
-              overflow-y: auto;
-              padding: 5rem 3rem 5rem 3rem;
-            }
-          }
-          @media screen and (max-width: 920px) {
-            flex-direction: column;
-
-            .pc-nav {
-              display: none;
-            }
-            .sp-nav {
-              position: fixed;
-              left: 0;
-              right: 0;
-              top: 0;
-              z-index: 100;
-              .header {
-                display: flex;
-                justify-content: space-between;
-                align-items: center;
-                width: 100vw;
-                height: 56px;
-                padding: 8px 16px 8px 16px;
-                background-color: rgb(189, 93, 56);
-                :global(.icon) {
-                  display: none !important;
-                }
-                .name {
-                  color: #fff;
-                  font-size: 1.25rem;
-                  font-weight: lighter;
-                }
-                .hamburger {
-                  width: 56px;
-                  height: 40px;
-                  background-color: transparent;
-                  border: 1px solid;
-                  border-color: rgba(255, 255, 255, 0.1);
-                  border-radius: 0.25em;
-                  cursor: pointer;
-                  padding: 0;
-                  :global(svg) {
-                    color: rgba(255, 255, 255, 0.55);
-                  }
-                }
-              }
-              .menu {
-                position: fixed;
-                top: 56px;
-                padding: 0 16px 0 16px;
-                width: 100%;
-                height: 160px;
-                background-color: rgb(189, 93, 56);
-                transition: height 0.1s;
-                &.collapsed {
-                  height: 0;
-                  overflow: hidden;
-                }
-              }
-            }
-            .main {
-              width: 100vw;
-              margin-top: 56px;
-              font-size: calc(0.5rem + 1.5vw);
-              padding: 2em;
-              min-height: calc(100vh - 56px);
-            }
-          }
-        }
-      `}</style>
     </>
   )
 }
