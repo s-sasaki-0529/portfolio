78a2cbf7bf10370c4f145caacb8420f250701208
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2024-01-22 23:02:59 +0900

    MenuItemListコンポーネントをリファクタ

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,31 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { useState } from 'react'
 
+/**
+ * メインメニュー内のリンク一覧
+ */
+const MenuItemList: React.FC<{ currentPageName: string }> = props => {
+  const itemList = ['about', 'outputs', 'experience']
+
+  return (
+    <ul className="p-0">
+      {itemList.map(item => (
+        <li key={item} className="h-[40px] font-extrabold list-none no-underline decoration-0 leading-[0]">
+          <Link href={`/${item}`}>
+            <a
+              className={`text-[1.1rem] text-gray-300 uppercase nonetex ${
+                props.currentPageName === item ? 'text-gray-50 underline' : ''
+              }`}
+            >
+              {item}
+            </a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
   const currentPageName = router.pathname.split('/')[1] || 'about'
@@ -16,33 +41,13 @@ function MyApp({ Component, pageProps }) {
     router.events.on('routeChangeComplete', () => setIsShowSpMenu(false))
   }
 
-  const Menu = () => (
-    <>
-      <ul className="py-2 px-0 m-0">
-        {['about', 'outputs', 'experience'].map(menu => (
-          <li key={menu} className="h-[40px] font-extrabold list-none no-underline decoration-0 leading-[0]">
-            <Link href={`/${menu}`}>
-              <a
-                className={`text-[1.1rem] text-gray-300 uppercase nonetex ${
-                  menu === currentPageName ? 'text-gray-50 underline' : ''
-                }`}
-              >
-                {menu}
-              </a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </>
-  )
-
   return (
     <>
       <div className="app flex">
         <Head>
           <title>Shingo Sasaki</title>
         </Head>
-        <nav className="flex flex-col justify-center items-center min-w-[270px] h-dvh bg-brand sp:hidden">
+        <nav className="flex flex-col justify-center items-center gap-5 min-w-[270px] h-dvh bg-brand sp:hidden">
           <Image
             src="/icon.jpg"
             alt="笹木信吾のいつものプロフィールアイコン画像"
@@ -50,8 +55,8 @@ function MyApp({ Component, pageProps }) {
             width={160}
             height={160}
           />
-          <div className="m-[2rem] text-center">
-            <Menu />
+          <div className="text-center">
+            <MenuItemList currentPageName={currentPageName} />
           </div>
         </nav>
         <nav className="sp-nav pc:hidden fixed left-0 right-0 top-0 z-[100]">
@@ -71,7 +76,7 @@ function MyApp({ Component, pageProps }) {
               isShowSpMenu ? 'h-[120px]' : 'h-0 overflow-hidden'
             }`}
           >
-            <Menu />
+            <MenuItemList currentPageName={currentPageName} />
           </div>
         </nav>
         <main className="pc:flex pc:items-center pc:w-full pc:h-dvh pc:overflow-y-auto pc:py-[5rem] pc:px-[3rem] sp:w-dvw sp:mt-[56px] sp:text-[calc(0.5rem+1.5vw)] sp:p-[2em] sp:min-h-[calc(100vh-56px)]">
